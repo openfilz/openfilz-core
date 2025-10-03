ad81e2f653e03baa21d89dd77cc38ccd13e1f49d
Saad Moumen <saad14092@gmail.com>
2025-10-03 16:01:50 +0200

    fix: ngx-env.js

--- openfilz-web/src/environments/environment.ts
+++ openfilz-web/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiURL: import.meta.env.NG_APP_API_URL,
-    graphQlURL: import.meta.env.NG_APP_GRAPHQL_URL
+    apiURL: import.meta.env['NG_APP_API_URL'],
+    graphQlURL: import.meta.env['NG_APP_GRAPHQL_URL']
 };
