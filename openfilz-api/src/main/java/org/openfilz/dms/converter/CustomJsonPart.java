67a62f196b412e227ad13997fccd418e4a9a6c23
Yann <yanndemel78@gmail.com>
2025-12-09 20:51:26 +0100

    Improve Swagger documentation and simplify /upload-multiple (content-type of parametersByFilename attribute does not need to be provided anymore)

---
+++ openfilz-api/src/main/java/org/openfilz/dms/converter/CustomJsonPart.java
@@ -0,0 +1,12 @@
+package org.openfilz.dms.converter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CustomJsonPart {
+    String value() default ""; // The part name
+}
