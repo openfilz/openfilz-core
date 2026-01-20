ba6e8ec116e24dbd249e594545f41694d41c9ccd
Yann <yanndemel78@gmail.com>
2026-01-20 16:56:50 +0100

    Dokploy files

---
+++ dokploy/openfilz-web/ngx-env.js
@@ -0,0 +1,9 @@
+globalThis._NGX_ENV_ = {
+    "NG_APP_API_URL": "https://api.openfilz.org/api/v1",
+    "NG_APP_GRAPHQL_URL": "https://api.openfilz.org/graphql/v1",
+    "NG_APP_AUTHENTICATION_ENABLED": "true",
+    "NG_APP_AUTHENTICATION_AUTHORITY": "https://auth.openfilz.org/realms/openfilz",
+    "NG_APP_AUTHENTICATION_CLIENT_ID": "openfilz-web",
+    "NG_APP_ONLYOFFICE_ENABLED": "true",
+    "NG_APP_ONLYOFFICE_URL": "https://docs.openfilz.org"
+};
